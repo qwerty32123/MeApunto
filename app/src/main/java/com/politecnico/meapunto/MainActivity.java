d9abd1e7e31bd062dd8f94e67b8562188a1852e9
127.0.0.1 localhost <qwerty97001@outlook.es>
2022-05-02 17:43:02 +0200

    Login Google

--- app/src/main/java/com/politecnico/meapunto/MainActivity.java
+++ app/src/main/java/com/politecnico/meapunto/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+         FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(this);
+         Bundle bundle = new Bundle();
+         bundle.putString("message","Integracion de Firebase completa");
+         analytics.logEvent("InitScreen",bundle);
+
     }
 }
