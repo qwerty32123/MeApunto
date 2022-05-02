804034cbb303f701276f79dfeae0ffe7f439eecd
127.0.0.1 localhost <qwerty97001@outlook.es>
2022-05-02 21:37:27 +0200

    mappeo clase usuario

---
+++ app/src/main/java/com/politecnico/meapunto/modelos/URLs.java
@@ -0,0 +1,8 @@
+package com.politecnico.meapunto.modelos;
+
+public class URLs {
+    private static final String ROOT_URL = "http://192.168.1.47/login/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
