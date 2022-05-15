7137648f3e6441c3f0b89cf8fa748b461071133a
127.0.0.1 localhost <qwerty97001@outlook.es>
2022-05-15 10:59:06 +0200

    Usuario editar informacion Inicio

---
+++ app/src/main/java/com/politecnico/meapunto/EditProfileActivity.java
@@ -0,0 +1,95 @@
+package com.politecnico.meapunto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+        //uper.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_edit_profile);
+        EditText etFirstName, etLastName, etEmail, etContactNo, etDec;
+    final int MIN_PASSWORD_LENGTH = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        viewInitializations();
+
+    }
+
+    void viewInitializations() {
+        etFirstName = findViewById(R.id.et_first_name);
+        etLastName = findViewById(R.id.et_last_name);
+        etEmail = findViewById(R.id.et_email);
+        etContactNo = findViewById(R.id.et_contact_no);
+        etDec = findViewById(R.id.et_des);
+
+        // To show back button in actionbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // Checking if the input in form is valid
+    boolean validateInput() {
+        if (etFirstName.getText().toString().equals("")) {
+            etFirstName.setError("Please Enter First Name");
+            return false;
+        }
+        if (etLastName.getText().toString().equals("")) {
+            etLastName.setError("Please Enter Last Name");
+            return false;
+        }
+        if (etEmail.getText().toString().equals("")) {
+            etEmail.setError("Please Enter Email");
+            return false;
+        }
+        if (etContactNo.getText().toString().equals("")) {
+            etContactNo.setError("Please Enter Contact No");
+            return false;
+        }
+        if (etDec.getText().toString().equals("")) {
+            etDec.setError("Please Enter Designation ");
+            return false;
+        }
+
+        // checking the proper email format
+        if (!isEmailValid(etEmail.getText().toString())) {
+            etEmail.setError("Please Enter Valid Email");
+            return false;
+        }
+
+        return true;
+    }
+
+    boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    // Hook Click Event
+
+    public void performEditProfile (View v) {
+        if (validateInput()) {
+
+            // Input is valid, here send data to your server
+
+            String firstName = etFirstName.getText().toString();
+            String lastName = etLastName.getText().toString();
+            String email = etEmail.getText().toString();
+            String contactNo = etContactNo.getText().toString();
+            String Designation = etDec.getText().toString();
+
+            Toast.makeText(this,"Profile Update Successfully",Toast.LENGTH_SHORT).show();
+            // Here you can call you API
+
+        }
+    }
+
+}
+
+
