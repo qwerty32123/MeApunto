d6469b24044deb80a69146daa166df9532b5bf26
127.0.0.1 localhost <qwerty97001@outlook.es>
2022-05-16 13:24:44 +0200

    Login Splash screen terminada

--- app/src/main/java/com/politecnico/meapunto/LoginSplashScreenActivity.java
+++ app/src/main/java/com/politecnico/meapunto/LoginSplashScreenActivity.java
@@ -8,11 +8,16 @@ import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.politecnico.meapunto.modelos.SharedPrefManager;
 import com.politecnico.meapunto.modelos.Usuario;
 
 public class LoginSplashScreenActivity extends AppCompatActivity {
     TextView usuarioTextView;
+
+
+    LottieAnimationView lottie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +30,27 @@ public class LoginSplashScreenActivity extends AppCompatActivity {
             usuarioTextView = findViewById(R.id.usuarioView);
             usuarioTextView.setText(user.getNombre());
 
-        }
-        finish();
+            lottie = findViewById(R.id.lotie);
 
-        startActivity(new Intent(getApplicationContext(), MenuPrincipal.class));
+
+
+            usuarioTextView.animate().translationY(-1200).setDuration(5000).setStartDelay(0);
+            lottie.animate().setDuration(2000).setStartDelay(2900);
+
+        }
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+
+                startActivity(new Intent(getApplicationContext(), MenuPrincipal.class));
+
+
+            }
+        },6000);
+
 
 
     }
